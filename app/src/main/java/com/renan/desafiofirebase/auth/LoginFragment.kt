4d983e056ea6105b3f9958a2d546908ad7b29301
Renan <jrferreirad@gmail.com>
2021-01-24 00:34:55 -0300

    implementando firebase

--- app/src/main/java/com/renan/desafiofirebase/auth/LoginFragment.kt
+++ app/src/main/java/com/renan/desafiofirebase/auth/LoginFragment.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import androidx.navigation.fragment.findNavController
 import com.renan.desafiofirebase.R
 
 
@@ -20,6 +22,17 @@ class LoginFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        createAccount()
+    }
+
+    private fun createAccount() {
+        val navControler = findNavController()
+        val btnSignup = view?.findViewById<Button>(R.id.btnSignup)
+
+        btnSignup?.setOnClickListener {
+            navControler.navigate(R.id.action_loginFragment_to_registerFragment)
+        }
 
     }
 }
