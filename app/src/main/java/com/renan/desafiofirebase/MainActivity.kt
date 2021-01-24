9b31b196abeb985521db8bcb0f8a38688812fd0d
Renan <jrferreirad@gmail.com>
2021-01-23 23:27:23 -0300

    criando tela login

---
+++ app/src/main/java/com/renan/desafiofirebase/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.renan.desafiofirebase
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.navigation.findNavController
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
---
+++ app/src/main/java/com/renan/desafiofirebase/splashscreen/SplashScreenActivity.kt
@@ -0,0 +1,22 @@
+package com.renan.desafiofirebase.splashscreen
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import com.renan.desafiofirebase.MainActivity
+import com.renan.desafiofirebase.R
+
+class SplashScreenActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }, 1500)
+
+    }
+}
