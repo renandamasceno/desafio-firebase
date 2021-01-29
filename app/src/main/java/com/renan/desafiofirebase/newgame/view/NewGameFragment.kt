adb12b442519cd33f3677736a96c12e3330ad793
Renan <jrferreirad@gmail.com>
2021-01-29 02:26:38 -0300

    app funcionando apenas problema com salvamento de imagem

--- app/src/main/java/com/renan/desafiofirebase/newgame/view/NewGameFragment.kt
+++ app/src/main/java/com/renan/desafiofirebase/newgame/view/NewGameFragment.kt
@@ -1,6 +1,7 @@
 package com.renan.desafiofirebase.newgame.view
 
 import android.Manifest
+import android.app.Activity
 import android.app.Activity.RESULT_OK
 import android.content.Intent
 import android.content.pm.PackageManager
@@ -9,6 +10,7 @@ import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
 import android.provider.MediaStore
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -105,10 +107,10 @@ class NewGameFragment : Fragment() {
         } else {
             ""
         }
-
+        Log.d("TAG", "gameImageUrl = $gameImageUrl")
         val game = GamesModel(
             gameId!!,
-            "gameImageUrl!!",
+            gameImageUrl!!,
             nameView.text.toString().toLowerCase(Locale.ROOT),
             createdAtView.text.toString(),
             descriptionView.text.toString()
@@ -173,6 +175,35 @@ class NewGameFragment : Fragment() {
                 listPermissionsNeeded.toTypedArray(),
                 requestCode
             )
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == READ_STORAGE_PERMISSION_CODE
+            && grantResults.contains(PackageManager.PERMISSION_GRANTED)
+        ) {
+            showImagePicker()
+        } else {
+            Toast.makeText(
+                _view.context,
+                "Permissão negada",
+                Toast.LENGTH_SHORT
+            ).show()
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (resultCode == Activity.RESULT_OK && requestCode == PICK_IMAGE_REQUEST_CODE && data!!.data != null) {
+            selectedImageUri = data.data
+
+            val profileImageView = _view.findViewById<ImageView>(R.id.imgCreateGameBtn)
+            Picasso.get().load(selectedImageUri).fit().centerCrop().into(profileImageView)
         }
     }
 
