4f70be6cb625d90a0736bcd990c951092a37a436
Renan <joserenan@softbuilder.com.br>
2021-01-29 11:38:44 -0300

    finalizando tela de detalhes

--- app/src/main/java/com/renan/desafiofirebase/gamedetails/DetailsGameFragment.kt
+++ app/src/main/java/com/renan/desafiofirebase/gamedetails/DetailsGameFragment.kt
@@ -8,6 +8,7 @@ import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
 import com.renan.desafiofirebase.R
+import com.squareup.picasso.Picasso
 
 
 class DetailsGameFragment : Fragment() {
@@ -30,19 +31,25 @@ class DetailsGameFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
         _view = view
         val gameId = arguments?.getString(GAME_ID)
-        val image= arguments?.getString(GAME_IMAGE)
+        val image = arguments?.getString(GAME_IMAGE)
         val name = arguments?.getString(GAME_NAME)
-        val dateLaunch = arguments?.getInt(GAME_DATE)
+        val dateLaunch = arguments?.getString(GAME_DATE)
         val description = arguments?.getString(GAME_DESCRIPTION)
 
-        val imageCoverView = _view.findViewById<ImageView>(R.id.imageGame)
-        val titleView = _view.findViewById<TextView>(R.id.nameGame)
-        val title2View = _view.findViewById<TextView>(R.id.name2game)
+        val imageView = _view.findViewById<ImageView>(R.id.imageGame)
+        val nameView = _view.findViewById<TextView>(R.id.nameGame)
+        val name2View = _view.findViewById<TextView>(R.id.name2game)
         val createdAtView = _view.findViewById<TextView>(R.id.dateGame)
         val descriptionView = _view.findViewById<TextView>(R.id.DescriptionGame)
+
+        Picasso.get().load(image).into(imageView)
+        name2View.text = name
+        createdAtView.text = dateLaunch
+        descriptionView.text = description
+
     }
 
-    companion object{
+    companion object {
         const val GAME_ID: String = "id"
         const val GAME_NAME: String = "title"
         const val GAME_DESCRIPTION: String = "description"
